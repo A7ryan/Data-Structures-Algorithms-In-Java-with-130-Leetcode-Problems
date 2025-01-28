3a4d2797bbf98c3e30ca27033b0562dc0981357c
A7ryan <136580189+A7ryan@users.noreply.github.com>
2025-01-28 15:04:13 +0530

    Added levelOrderTraversal

--- tree/treeWithTraversal.cpp
+++ tree/treeWithTraversal.cpp
@@ -71,7 +71,33 @@ void postOrderTraversal(Tree* tree) {
 
 // Level - wise
 void levelOrderTraversal(Tree* tree) {
+    vector<vector<int>> ans;
+    if(tree == nullptr) return;
+    queue<Tree*> q;
+    q.push(tree);
 
+    while(!q.empty()) {
+        int size = q.size();
+        vector<int> level;
+        for(int i = 0; i < size; i++) {
+            Tree* tree = q.front();
+            q.pop();
+            level.push_back(tree->getData());
+
+            if(tree->getLeft() != nullptr) {
+                q.push(tree->getLeft());
+            }
+            if(tree->getRight() != nullptr) {
+                q.push(tree->getRight());
+            }
+        }
+        ans.push_back(level);
+
+        for(auto it: level) {
+            cout << it << " ";
+        }
+        cout << endl;
+    }
 }
 
 
@@ -79,20 +105,30 @@ int main() {
     Tree* rootNode = new Tree(10);
     Tree* tree2 = new Tree(20);
     Tree* tree3 = new Tree(30);
+    
+    // Tree* tree4 = new Tree(40);
+    // Tree* tree5 = new Tree(50);
+    // Tree* tree6 = new Tree(60);
+    // Tree* tree7 = new Tree(70);
 
     rootNode->setLeft(tree2);
+    // tree2->setLeft(tree4);
+    // tree2->setRight(tree5);
+
     rootNode->setRight(tree3);
+    // tree3->setLeft(tree6);
+    // tree3->setRight(tree7);
 
     // cout << rootNode->getData() << endl;
     // cout << rootNode->getLeft()->getData() << endl;
     // cout << rootNode->getRight()->getData() << endl;
 
-    inOrderTraversal(rootNode);
-    cout << endl;
-    preOrderTraversal(rootNode);
-    cout << endl;
-    postOrderTraversal(rootNode);
-    cout << endl;
-    
+    // inOrderTraversal(rootNode);
+    // cout << endl;
+    // preOrderTraversal(rootNode);
+    // cout << endl;
+    // postOrderTraversal(rootNode);
+    // cout << endl;
+    levelOrderTraversal(rootNode);
     return 0;
 }
